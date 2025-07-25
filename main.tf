f3f2e27f35e65add31a16c3f3114c8c8ee812717
Priti09-lab <priti.nakade99@gmail.com>
2025-07-25 14:59:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-
-resource "fabric_workspace" "example" {
+resource "fabric_workspace" "workspace" {
   display_name = var.workspace_display_name
   location     = var.location
   capacity_id  = var.capacity_id
