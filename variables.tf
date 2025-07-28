0503ad504f0d9bc3e01fe250fd63fb15fcb4c5af
Priti09-lab <priti.nakade99@gmail.com>
2025-07-28 15:36:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,3 @@
-variable "tenant_id" {
-  type        = string
-  description = "Azure Entra ID Tenant ID"
-}
-
-variable "subscription_id" {
-  type        = string
-  description = "Azure Subscription ID"
-}
 
 variable "capacity_id" {
   type        = string
@@ -16,14 +7,4 @@ variable "capacity_id" {
 variable "workspace_display_name" {
   type        = string
   description = "Display name for the Fabric workspace"
-}
-
-variable "workspace_members" {
-  type        = list(string)
-  description = "List of user principal names to add to the workspace"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region"
 }
